10a5bd21fdbf70e6ab7b968d217b37a9d9518802
mncastillo-glitch <mncastillo@tip.edu.ph>
2026-04-16 09:43:44 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,16 @@
 <?php
+// Get connection settings from Railway Environment Variables
 $hostname = getenv('DB_HOST');
 $username = getenv('DB_USER');
 $password = getenv('DB_PASS');
 $database = getenv('DB_NAME');
 $port     = getenv('DB_PORT');
 
+// Connect using the specific Aiven port (15368)
 $conn = mysqli_connect($hostname, $username, $password, $database, $port);
 
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    // This will help us see if the connection is still failing
+    die("Database Connection failed: " . mysqli_connect_error());
 }
 ?>
