0609b13f3f2a24a05d8256338fdfe9a2b738784d
mncastillo-glitch <mncastillo@tip.edu.ph>
2026-04-17 11:23:21 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,17 @@
 <?php
-// Only one connection attempt is needed using getenv()
-$conn = mysqli_connect(
-    getenv('DB_HOST'), 
-    getenv('DB_USER'), 
-    getenv('DB_PASS'), 
-    getenv('DB_NAME'), 
-    getenv('DB_PORT')
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL); // enables SSL without cert verification
+
+mysqli_real_connect(
+    $conn,
+    getenv('DB_HOST'),
+    getenv('DB_USER'),
+    getenv('DB_PASS'),
+    getenv('DB_NAME'),
+    getenv('DB_PORT'),
+    NULL,
+    MYSQLI_CLIENT_SSL
 );
 
 if (!$conn) {
